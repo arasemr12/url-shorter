e0c97cb7a1da5df76765f576aeac3f18179afcbc
Glitch (link-kisalt12) <none>
2021-07-13 01:16:50 +0000

    v0.0.2

--- index.js
+++ index.js
@@ -1,112 +1,85 @@
-const express = require('express');
+const express = require("express");
 const expresslayouts = require("express-ejs-layouts");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { JsonDatabase } = require("wio.db");
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-require('dotenv').config();
 
-//database yolu.
 const db = new JsonDatabase({
-    databasePath: "./databases/database.json"
+  databasePath: "./databases/database.json"
 });
 
-client.on('ready', () => {
-    server.listen(3000);
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity(`0 Kişi!`, { type: 'WATCHING' });
-})
+client.on("ready", () => {
+  server.listen(3000);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(`0 Kişi!`, { type: "WATCHING" });
+});
 
-//css veya js eklemek için statik kısmı.
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-//ip leri kaydetmek için trust proxy.
-app.set('trust proxy', true)
+app.set("trust proxy", true);
 
-//verilerin gönderilebilmesi için bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//ejs layouts kullanımı.
 app.use(expresslayouts);
-app.set("layout", "./layouts/full-width");
+app.set("layout", "./layouts/main");
 
-//ejs motor.
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-//anasayfa
-app.get('/', (req, res) => {
-    res.render('index', { title: 'link-kisalt - Home' });
+app.get("/", (req, res) => {
+  res.render("index", { title: "FEYZ - Home" });
 });
 
-//api doc.
-app.get('/api', (req, res) => {
-    res.render('api', { title: 'link-kisalt - Api' });
+app.get("/api", (req, res) => {
+  res.render("api", { title: "FEYZ - Api" });
 });
 
 let online_user_count = 0;
 
-//socket.io'ya kullanıcı bağlandığında.
 io.on("connection", socket => {
-    online_user_count++;
-    client.user.setActivity(`${online_user_count} Kişi!`, { type: 'WATCHING' });
-    //socket io kullanıcı çıkış.
-    socket.on("disconnect", () => {
-        online_user_count--;
-        client.user.setActivity(`${online_user_count} Kişi!`, { type: 'WATCHING' });
-    });
+  online_user_count++;
+  client.user.setActivity(`${online_user_count} Kişi!`, { type: "WATCHING" });
+  socket.on("disconnect", () => {
+    online_user_count--;
+    client.user.setActivity(`${online_user_count} Kişi!`, { type: "WATCHING" });
+  });
 });
 
-app.post('/', (req, res) => {
-    const { url, id } = req.body;
+app.post("/", (req, res) => {
+  const { url, id } = req.body;
+  if (url && id) {
     if (db.has(id)) {
-        //id daha önceden kaydedildimi kontrol.
-        res.send('Bu id daha önceden alınmış!');
+      res.render("index", { title: "FEYZ - Home", message: "This id is already taken!" });
     } else {
-        //id kaydedilmediyse id yi girilen url ile database'ye kaydetme.
-        db.set(`${id}.url`, url);
-        db.set(`${id}.id`, id);
-        db.set(`${id}.ip`, req.ip);
-        res.redirect(`/${id}`);
+      db.set(`${id}`, {
+        url: url,
+        id: id,
+        ip: req.ip
+      });
+      res.redirect(`/${id}`);
     }
+  }else {
+    res.redirect("/");
+  }
 });
 
-app.get('/:id', (req, res) => {
-    const { id } = req.params;
-    //id girildimi kontrol.
-    if (id) {
-        if (db.has(id)) {
-            //id database'ye kayıtlıysa url'e yönlendirme.
-            res.redirect(db.get(id).url)
-        } else {
-            //id database'ye kayıtlı değilse ana sayfaya yönlendirme.
-            res.redirect('/');
-        }
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  if (id) {
+    if (db.has(id)) {
+      res.render("url", { title: `FEYZ - ${id}`, veri: db.get(id) });
     } else {
-        res.redirect('/');
+      res.redirect("/");
     }
+  } else {
+    res.redirect("/");
+  }
 });
 
-app.get('/api/:id', (req, res) => {
-    const { id } = req.params;
-    //id girildimi kontrol.
-    if (id) {
-        if (db.has(id)) {
-            //id database'ye kayıtlıysa veri gönderme.
-            res.json(db.get(id));
-        } else {
-            //id database'ye kayıtlı değilse ana sayfaya yönlendirme.
-            res.redirect('/');
-        }
-    } else {
-        res.redirect('/');
-    }
-});
-
-//Bot giriş.
-client.login(process.env.token);
+client.login(process.env.TOKEN);
